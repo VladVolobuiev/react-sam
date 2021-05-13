d583a80d9a9a058e5eaa5380253fd8b57bdd4338
VladVolobuiev <72812114+VladVolobuiev@users.noreply.github.com>
2021-05-13 13:49:25 +0300

    Updates

--- src/components/Dialogs/Message/Message.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './../Dialogs.module.css';
-
-const Message = (props) => {
-  return <div className={s.dialogM}>{props.message}</div>;
-};
-
-export default Message;
